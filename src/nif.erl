02b9a3eb025b4cb2f8765cc86515138b6aba7278
LiChunhui <chunhui_true@163.com>
2013-09-29 05:37:20 +0800

    x

---
+++ src/nif.erl
@@ -0,0 +1,57 @@
+%% Copyright LiChunhui (chunhui_true@163.com)
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+
+
+
+-module(packet).
+
+-export([open_nic/1]).
+-export([close_nic/1]).
+-export([read_nic/1]).
+-export([write_nic/2]).
+
+-on_load(init/0).
+
+-define(nif_stub, nif_stub_error(?LINE)).
+nif_stub_error(Line) ->
+    erlang:nif_error({nif_not_loaded,module,?MODULE,line,Line}).
+
+
+init() ->
+    PrivDir = case code:priv_dir(?MODULE) of
+                  {error, bad_name} ->
+                      EbinDir = filename:dirname(code:which(?MODULE)),
+                      AppPath = filename:dirname(EbinDir),
+                      filename:join(AppPath, "priv");
+                  Path ->
+                      Path
+              end,
+    erlang:load_nif(filename:join(PrivDir, "stacko_drv"), 0).
+
+
+open_nic(Name) when is_atom(Name) ->
+    ?nif_stub.
+
+
+close_nic(Name) when is_atom(Name) ->
+    ?nif_stub.
+
+
+read_nic(Index) when is_integer(Index) ->
+    ?nif_stub.
+
+
+write_nic(Index, Packet) when is_integer(Index), 
+                              is_binary(Packet) ->
+    ?nif_stub.
