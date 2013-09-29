059c91d1dac9cf2cebafe55cc5f937e78b39fe58
LiChunhui <chunhui_true@163.com>
2013-09-29 16:28:11 +0800

    x

--- src/stacko_sup.erl
+++ src/stacko_sup.erl
@@ -23,13 +23,13 @@
 
 
 start_link() ->
-    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+    {ok, Pid} = supervisor:start_link({local, ?MODULE}, ?MODULE, []),
+    %% init tables
+    tables:create_ip_table(),
+    {ok, Pid}.
     
 
 init([]) ->
-    %% init tables
-    tables:create_ip_table(),
-
     SuperSpec = {one_for_one, 5, 5},
     IpSpec = {ip_sup,                    % id
                {ip_sup, start_link, []}, % {Module, Function, Arguments}
